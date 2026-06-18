904e56e0d57c659aa85f6cd8151dede413159b9d
rajshrees093-ai <rajshrees093@gmail.com>
2026-06-18 18:57:29 +0530

    commit

---
+++ madlib_game.py
@@ -0,0 +1,12 @@
+#madlib game : it is a word game where you create stories by filling in blanks with random words
+
+adjective1=input("enter the adjective (description): ")
+noun1=input("enter the noun (person/place/thing): ") 
+adjective2=input("enter the adjective (description): ")
+verb1=input("enter the verb (in present tense ie ending with ing): ") 
+adjective3=input("enter the adjective (description): ")
+
+print(f"today i went to a {adjective1}  zoo")
+print(f"in an exhibit, I saw  {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
---
+++ shopping_cart_Program.py
@@ -0,0 +1,5 @@
+item= input("enter the name of the item:")
+price=float(input("enter the proce for the item:"))
+quantity=int(input("enter the quantity:"))
+total=price*quantity
+print(total)
